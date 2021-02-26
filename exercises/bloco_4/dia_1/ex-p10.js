b852e5c698ab259fe636eb8c93ece99c077f6229
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-02-25 21:59:06 -0300

    Altera lógica do exercício prático 10

--- exercises/bloco_4/dia_1/ex-p10.js
+++ exercises/bloco_4/dia_1/ex-p10.js
@@ -1,12 +1,13 @@
-let valorDeCusto = 10;
-let valorDeVenda = -9;
+let valorDeCusto = 5000;
+let valorDeVenda = 9000;
+let lucro = valorDeVenda-(1.2*valorDeCusto);
 
 
-if(valorDeVenda>valorDeCusto*1.2) {
-    let lucro = valorDeVenda-1.2*valorDeCusto;
-    console.log(lucro);
-} else if (valorDeCusto<0 || valorDeVenda<0) {
-    console.log("ERROR: Dados Inválidos: Os valores devem ser positivos!"); 
+if (valorDeCusto<0 || valorDeVenda<0) {
+    console.log("ERROR: Dados Inválidos: Os valores devem ser positivos!");
+} 
+else if(valorDeVenda>(valorDeCusto*1.2)) {
+    console.log(lucro*1000);
 } else {
-    console.log("Não terá lucro com esse valor estipulado para a venda!! Melhor segurar!!")
+    console.log("Não terá lucro com esse valor estipulado para a venda!! Melhor segurar!!");
 }

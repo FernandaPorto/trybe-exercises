ccf6fa0930c2c6cb8b3d0600c65dbc2cad84e6b9
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-02-27 16:25:46 -0300

    Adiciona pasta do dia 3 e exercicio 1 do bloco 4.3

---
+++ exercises/bloco_4/dia_3/ex-p1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let inputLine = '';
+let asterisc = '*'
+
+for (let index = 0; index<5; index += 1){
+    inputLine = inputLine + asterisc;
+}
+
+for (let colum = 0; colum<n; colum+=1){
+    console.log(inputLine);
+}

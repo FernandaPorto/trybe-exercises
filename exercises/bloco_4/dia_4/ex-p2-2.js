725623af489a452b357d7a18eedd4ad2f0638314
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-02 00:01:14 -0300

    Adiciona alteracoes as questoes anteriores, bem como inicia a questao 5

--- exercises/bloco_4/dia_4/ex-p2-2.js
+++ exercises/bloco_4/dia_4/ex-p2-2.js
@@ -10,3 +10,16 @@ function maiorDoArray([number1, number2, number3, number4, number5, number6]){
 }
 
 console.log(maiorDoArray([2,3,6,7,10,1]));
+
+// De acordo com o gabarito - eu nao queria ter olhado, mas fiquei curiosa se nao havia forma mais facil/resumida de se fazer este exercicio - pode-se utilizar aqui um objeto para definir os numeros e utilizar o for-in para localizar a propriedade desejada. Segue tentativa de resolucao seguindo esta ideia (fiz sem olhar o gabarito novamente!):
+
+function higherArrayNumber(number){
+    let higherNumber = 0;
+    for(let indice in number){
+        if(number[higherNumber] < number[indice]){
+            higherNumber = indice;
+        }
+    }
+    return higherNumber;
+}
+console.log(higherArrayNumber([2,3,6,7,10,1]));
---
+++ exercises/bloco_4/dia_4/ex-p2-5.js
@@ -0,0 +1,6 @@
+function maiorRepeticao(numeros) {
+    let maiorRepeticao = numeros[0];
+    for (let key in numeros){
+        for (let index = 0; index < )
+    }
+}

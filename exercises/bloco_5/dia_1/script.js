16f0c5e746973376894b03938b2d7c9e85402c01
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-04 18:26:32 -0300

    Adiciona exercicio de conceito do bloco 5.1

---
+++ exercises/bloco_5/dia_1/script.js
@@ -0,0 +1,14 @@
+    var paragraph = document.getElementById("paragraph");
+        paragraph.style.color = "gold";
+
+    document.getElementById("page-title").innerText = 'Death proof'
+
+    var secondParagraph = document.getElementById("second-paragraph");
+        secondParagraph.style.backgroundColor = "blue";
+        secondParagraph.style.color = 'white';
+        secondParagraph.style.height = '10px';
+        secondParagraph.style.textAlign = 'center';
+        secondParagraph.style.padding = '20px';
+        secondParagraph.style.borderRadius = '30px';
+
+    document.getElementById("subtitle").innerText = "Fernanda"

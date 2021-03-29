871679ff039fd80d525d5aa274558412e2b71f2a
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-29 16:05:23 -0300

    fixacao 4

--- exercises/bloco_8/exercicio-de-fixacao-4.js
+++ exercises/bloco_8/exercicio-de-fixacao-4.js
@@ -1,5 +1,6 @@
 const greaterThan = (firstNumber) => (secondNumber) => secondNumber > firstNumber;
+console.log(greaterThan(15)(15));
 
-const greaterThan10 = greaterThan(5);
-console.log(greaterThan10(3));
-// true
+// const greaterThan10 = greaterThan(10);
+// console.log(greaterThan10(15));
+
